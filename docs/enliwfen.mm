44a3b585b563bdce9febccdeadcaebfa98b936a7
hwirths <18348322+hwirths@users.noreply.github.com>
2026-04-03 10:02:00 +0200

    project update
    The project has been converted into a nodejs project.

---
+++ docs/enliwfen.mm
@@ -0,0 +1,541 @@
+<map version="freeplane 1.12.15">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<bookmarks/>
+<node TEXT="enliwfen" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1772873228119" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_icon_for_attributes="true" auto_compact_layout="true" show_tags="UNDER_NODES" associatedTemplateLocation="template:/standard-1.6.mm" show_note_icons="true" fit_to_viewport="false" show_icons="BESIDE_NODES" showTagCategories="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="6" RULE="ON_BRANCH_CREATION"/>
+<richcontent TYPE="DETAILS" HIDDEN="true">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Enliven web sites rendered server side
+    </p>
+    <p>
+      as for example done by Django.
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="document fragment loading" POSITION="bottom_or_right" ID="ID_241920319" CREATED="1772873012070" MODIFIED="1772873849618">
+<edge COLOR="#ff0000"/>
+<richcontent TYPE="DETAILS" HIDDEN="true">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Allow loading several small views into a single page
+      </li>
+      <li>
+        Allow form processing on a single page
+      </li>
+      <li>
+        Allow view updates on a single page
+      </li>
+      <li>
+        Provide polling for view updates
+      </li>
+      <li>
+        ...
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<node TEXT="targets" ID="ID_1937435060" CREATED="1772955749607" MODIFIED="1772955761227">
+<node TEXT="detail views" POSITION="bottom_or_right" ID="ID_473606652" CREATED="1772873058317" MODIFIED="1772873069260"/>
+<node TEXT="list views" POSITION="bottom_or_right" ID="ID_1059674896" CREATED="1772873069524" MODIFIED="1772873073375"/>
+<node TEXT="form views" POSITION="bottom_or_right" ID="ID_1944511141" CREATED="1772873073531" MODIFIED="1772873076433"/>
+<node TEXT="form elements" POSITION="bottom_or_right" ID="ID_1297409687" CREATED="1772873077349" MODIFIED="1772874134125"/>
+<node TEXT="dialogs/modals" POSITION="bottom_or_right" ID="ID_1375245130" CREATED="1772874135412" MODIFIED="1772874208497"/>
+<node TEXT="..." POSITION="bottom_or_right" ID="ID_1961758199" CREATED="1772874210527" MODIFIED="1772874212616"/>
+</node>
+<node TEXT="events" ID="ID_844695578" CREATED="1772955799203" MODIFIED="1772957490141">
+<arrowlink DESTINATION="ID_1250837068"/>
+<node TEXT="before" ID="ID_625839024" CREATED="1772955814471" MODIFIED="1772957472270"/>
+<node TEXT="done" ID="ID_1097502121" CREATED="1772955825032" MODIFIED="1772957474838"/>
+<node TEXT="accepted" ID="ID_11833167" CREATED="1772955832406" MODIFIED="1772957479546"><richcontent TYPE="DETAILS" HIDDEN="true">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      The server accepted the request and will process it.
+    </p>
+    <p>
+      The result may come in by polling, by a server sent
+    </p>
+    <p>
+      event or through a web socket connection.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="failed" ID="ID_1109322924" CREATED="1772956801719" MODIFIED="1772957482661"/>
+</node>
+<node TEXT="responses" ID="ID_96680299" CREATED="1772955904918" MODIFIED="1772958872295">
+<node TEXT="" ID="ID_1714615714" CREATED="1772957280636" MODIFIED="1772957280636">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="200_OK" ID="ID_473241353" CREATED="1772955924104" MODIFIED="1772957434240"/>
+<node TEXT="201_CREATED" ID="ID_1055219171" CREATED="1772955936377" MODIFIED="1772957437919"/>
+<node TEXT="202_ACCEPTED" ID="ID_1727678754" CREATED="1772956049451" MODIFIED="1772957441848"/>
+<node TEXT="203_NON-AUTHORITATIVE INFORMATION" ID="ID_337533658" CREATED="1772956060969" MODIFIED="1772957447678"/>
+<node TEXT="" ID="ID_947531294" CREATED="1772957280621" MODIFIED="1772957280635">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="successful requests with content" ID="ID_458848083" CREATED="1772957280646" MODIFIED="1772957466164">
+<arrowlink DESTINATION="ID_774049873"/>
+<arrowlink DESTINATION="ID_1097502121"/>
+</node>
+</node>
+<node TEXT="204_NO_CONTENT" ID="ID_5807452" CREATED="1772956080543" MODIFIED="1772957218904">
+<arrowlink DESTINATION="ID_1097502121"/>
+</node>
+<node TEXT="205_RESET_CONTENT" ID="ID_1802084285" CREATED="1772956094027" MODIFIED="1773039200635"><richcontent TYPE="DETAILS" HIDDEN="true">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      What should be done here?
+    </p>
+    <p>
+      This status code is not meant to refresh
+    </p>
+    <p>
+      the entire page. Instead it is meant to
+    </p>
+    <p>
+      reset for example a form.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="500_INTERNAL_SERVER_ERROR" ID="ID_720870093" CREATED="1772956122468" MODIFIED="1772957216817">
+<arrowlink DESTINATION="ID_1109322924"/>
+</node>
+<node TEXT="Remaining codes" ID="ID_1433535825" CREATED="1772956142650" MODIFIED="1772956910654">
+<arrowlink DESTINATION="ID_1109322924"/>
+</node>
+</node>
+</node>
+<node TEXT="DOM features" POSITION="bottom_or_right" ID="ID_190045104" CREATED="1772911192282" MODIFIED="1772911203692">
+<edge COLOR="#00ff00"/>
+<node TEXT="element replacement" ID="ID_774049873" CREATED="1772911207388" MODIFIED="1772957555646">
+<arrowlink DESTINATION="ID_1937435060" STARTARROW="NONE" ENDARROW="NONE"/>
+<richcontent TYPE="DETAILS" HIDDEN="true">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Element replacement can be done
+    </p>
+    <p>
+      by element id. The root element of
+    </p>
+    <p>
+      the loaded document fragment has
+    </p>
+    <p>
+      the same id as an element of the
+    </p>
+    <p>
+      active document. The element can
+    </p>
+    <p>
+      then be replaced.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Instead of placing the id in the root
+    </p>
+    <p>
+      element of the loaded document fragment,
+    </p>
+    <p>
+      the target id can be specified at the element
+    </p>
+    <p>
+      launching the loading of the document fragment.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Rules need to be defined:
+    </p>
+    <ul>
+      <li>
+        The new element has an id corresponding to an<br/>element of the active document: The corresponding<br/>element of the active document will be replaced by<br/>the new element.
+      </li>
+      <li>
+        The new element has an id without a<br/>corresponding element of the active document:
+
+        <ul>
+          <li>
+            The&#xa0;launching element has defined a target<br/>element, which is part of the active document:<br/>The target element will be replaced by the<br/>new element.
+          </li>
+          <li>
+            The launching element has defined more than<br/>one target element: The first target element<br/>will be replaced by the new element.
+          </li>
+          <li>
+            The launching element has not defined a<br/>target element: The launching element will<br/>be replaced by the new element.
+          </li>
+        </ul>
+      </li>
+      <li>
+        The new element has no id set:
+
+        <ul>
+          <li>
+            The launching element has defined a target<br/>element, which is part of the active document:<br/>The target element will be replaced by the<br/>new element.
+          </li>
+          <li>
+            The launching element has defined more than<br/>one target element: The target elements will<br/>be replaced by the new element.
+          </li>
+          <li>
+            The launching element has not defined a<br/>target element: The launching element will<br/>be replaced by the new element.
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="multiple element replacements" ID="ID_1240489517" CREATED="1772912194031" MODIFIED="1772912224455"/>
+<node TEXT="content reset of the entire page" ID="ID_1692536246" CREATED="1772912229564" MODIFIED="1772912253140"/>
+<node TEXT="element insertion?" ID="ID_1800895599" CREATED="1772912015684" MODIFIED="1772958799366"/>
+<node TEXT="element removement?" ID="ID_1478665466" CREATED="1772912031801" MODIFIED="1772912040936"/>
+<node TEXT="sanitization" ID="ID_820554232" CREATED="1773039254635" MODIFIED="1773039536001"><richcontent TYPE="DETAILS" HIDDEN="true">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Sanitization of the content returned by the
+    </p>
+    <p>
+      server is planned to be sanitized using
+    </p>
+    <p>
+      DOMPurify.
+    </p>
+    <p>
+      The main questions here:
+    </p>
+    <p>
+      What about a customized configuration,
+    </p>
+    <p>
+      which could be contrary to the needs
+    </p>
+    <p>
+      of enliwfen? Required tags and attributes
+    </p>
+    <p>
+      have to be documented, in order to guide
+    </p>
+    <p>
+      users.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="UI features" POSITION="top_or_left" ID="ID_834397084" CREATED="1772873354807" MODIFIED="1772873363717">
+<edge COLOR="#0000ff"/>
+<node TEXT="action indication" ID="ID_1250837068" CREATED="1772873364829" MODIFIED="1772958456494"><richcontent TYPE="DETAILS" HIDDEN="true">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Show indications when loading
+    </p>
+    <p>
+      document fragments, for example
+    </p>
+    <p>
+      spinners.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Events:
+    </p>
+    <ul>
+      <li>
+        before: The action indication is shown
+      </li>
+      <li>
+        done, failed: The action indication is closed
+      </li>
+    </ul>
+    <p>
+      An action indicator may be bound to more
+    </p>
+    <p>
+      than one launching element. Therefore a counter
+    </p>
+    <p>
+      is needed, in order to decide whether the action
+    </p>
+    <p>
+      indicator can be closed when receiving either
+    </p>
+    <p>
+      the event &quot;done&quot; or &quot;failed&quot;.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      A challenge may be given to use an action
+    </p>
+    <p>
+      indicator with document updates running through
+    </p>
+    <p>
+      a &quot;before&quot;, &quot;accepted&quot; and &quot;done&quot;/&quot;failed&quot; event
+    </p>
+    <p>
+      chain, especially when the updated document
+    </p>
+    <p>
+      will be fetched by polling.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="attribute/class toggling" ID="ID_1544333564" CREATED="1772873646357" MODIFIED="1772873665452">
+<node TEXT="collapsing" ID="ID_269323032" CREATED="1772873504768" MODIFIED="1772912371520">
+<arrowlink DESTINATION="ID_473606652" STARTINCLINATION="857.99997 pt;-193.49999 pt;" ENDINCLINATION="920.99997 pt;-193.49999 pt;" STARTARROW="NONE" ENDARROW="NONE"/>
+<richcontent TYPE="DETAILS" HIDDEN="true">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Provide collapsing to create
+    </p>
+    <p>
+      for example accordions or tabs.
+    </p>
+    <p>
+      Thereby offer the possibility to load
+    </p>
+    <p>
+      the collapsed content on first
+    </p>
+    <p>
+      show request.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="dialogs/modals" ID="ID_87550176" CREATED="1772873686302" MODIFIED="1772874429496"><richcontent TYPE="DETAILS" HIDDEN="true">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Open/Close dialogs/modals present in the document
+      </li>
+      <li>
+        Load dialogs/models on open request
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="input enabling/disabling" ID="ID_1063921467" CREATED="1772873752834" MODIFIED="1772873767351"/>
+</node>
+<node TEXT="form control" ID="ID_478338851" CREATED="1772873793975" MODIFIED="1772873928999">
+<node TEXT="checkbox groups" ID="ID_1082095014" CREATED="1772873931114" MODIFIED="1772873936806"/>
+<node TEXT="input enabling/disabling" ID="ID_103634854" CREATED="1772873943261" MODIFIED="1772875242447">
+<arrowlink DESTINATION="ID_1063921467" STARTARROW="NONE" ENDARROW="NONE"/>
+<arrowlink DESTINATION="ID_1297409687" STARTINCLINATION="791.99998 pt;0 pt;" ENDINCLINATION="793.49998 pt;257.24999 pt;" STARTARROW="NONE" ENDARROW="NONE"/>
+</node>
+<node TEXT="selection updates" ID="ID_1954866781" CREATED="1772874007578" MODIFIED="1772875135276">
+<arrowlink DESTINATION="ID_1297409687" STARTINCLINATION="662.99998 pt;191.24999 pt;" ENDINCLINATION="981.74997 pt;72 pt;" STARTARROW="NONE" ENDARROW="NONE"/>
+<richcontent TYPE="DETAILS" HIDDEN="true">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Selection updates are for example subject
+    </p>
+    <p>
+      to reduce selection lists with respect to a
+    </p>
+    <p>
+      current input, especially when the selection
+    </p>
+    <p>
+      list is not directly filtered by the input.
+    </p>
+    <p>
+      The challenge here is the combination
+    </p>
+    <p>
+      of an input element and the
+    </p>
+    <p>
+      selection list.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="data lists?" ID="ID_1743783098" CREATED="1772874017347" MODIFIED="1772874948555"><richcontent TYPE="DETAILS" HIDDEN="true">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      It is unclear, when data lists could be
+    </p>
+    <p>
+      subject to dynamic updates.
+    </p>
+    <p>
+      Possibly in cases, where filling the
+    </p>
+    <p>
+      data list might be too costy and it
+    </p>
+    <p>
+      gets worth to get reduced excerpts
+    </p>
+    <p>
+      based on the current input.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node TEXT="Navigation history?" POSITION="top_or_left" ID="ID_940358143" CREATED="1772912337343" MODIFIED="1772912357693">
+<edge COLOR="#ff00ff"/>
+</node>
+<node TEXT="SSE" POSITION="bottom_or_right" ID="ID_670604341" CREATED="1772955501489" MODIFIED="1772955506070">
+<edge COLOR="#00ffff"/>
+<node TEXT="notifications" ID="ID_1536941604" CREATED="1772955577261" MODIFIED="1772955585532"/>
+<node TEXT="document updates" ID="ID_184089568" CREATED="1772955586056" MODIFIED="1772957573918">
+<arrowlink DESTINATION="ID_774049873"/>
+<richcontent TYPE="DETAILS" HIDDEN="true">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Document updates may come in triggered either
+    </p>
+    <p>
+      by server internal events or&#xa0;by previous requests.
+    </p>
+    <p>
+      Receiving document updates will cause the dispatch
+    </p>
+    <p>
+      of an update event.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="Web socket" POSITION="top_or_left" ID="ID_940643966" CREATED="1772956501404" MODIFIED="1772956511443">
+<edge COLOR="#7c0000"/>
+</node>
+</node>
+</map>
